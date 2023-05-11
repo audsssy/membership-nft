ef9b51c3ba97b8a456f14a212ce3fafb9f409401
audsssy <stamfordh@hotmail.com>
2023-05-11 12:11:20 -0700

    Update BZDAccessList.sol

--- contracts/BZDAccessList.sol
+++ contracts/BZDAccessList.sol
@@ -2,56 +2,65 @@
 
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/utils/Multicall.sol";
+
 interface IBZDMembershipDirectory {
     function isMember(address account) external view returns (bool);
 }
 
 interface IBZDMembershipNFT {
-    function mintAndAddMembersToSeason(address[] calldata members, uint256 seasonId) external ;
+    function mintAndAddMembersToSeason(address member, uint256 seasonId) external ;
 }
 
 struct List {
+    bool open;
     uint256 fee;
     address[] list;
-    mapping( address => bool) listed;
+    mapping(address => bool) listed;
 }
 
 /// @title BZDWhitelist
 /// @notice BZD access manager
 /// credit: https://github.com/kalidao/kali-contracts/blob/main/contracts/access/KaliAccessManager.sol
-contract BZDAccessList {
-    /// -----------------------------------------------------------------------
-    /// Events
-    /// -----------------------------------------------------------------------
-
-    // event ListCreated(address indexed operator, uint256 id);
-    // event AccountListed(address indexed account, uint256 id, bool approved);
-
+contract BZDAccessList is Multicall {
     /// -----------------------------------------------------------------------
     /// Errors
     /// -----------------------------------------------------------------------
 
     error Unauthorized();
+
     error IncorrectAmount();
+
+    error MintHasNotStarted();
+
     error NotListed();
-    error NothingToRemove();
 
+    error NoAccountToRemove();
+
+    error NoAccountToAdd();
+
+    error WithdrawalFailed();
+
+    error ListNotFound();
+    
     /// -----------------------------------------------------------------------
-    /// List Storage
+    /// Storage
     /// -----------------------------------------------------------------------
 
-    // admins directory
+    /// @dev BZDMembershipDirectory contract.
     IBZDMembershipDirectory public admins;
+    
+    /// @dev BZDMembershipNFT contract.
     IBZDMembershipNFT public nft;
 
     uint256 public listCount;
-    mapping(uint256 => address[]) lists;
-    mapping(uint256 => uint256) listMintFee;
-    mapping(uint256 => mapping(address => bool)) listed;
+    
+    mapping(uint256 => List) public lists;
 
-    /**
-     * @dev Modifier to check that the caller is an admin.
-     */
+    /// -----------------------------------------------------------------------
+    /// Modifier
+    /// -----------------------------------------------------------------------
+
     modifier onlyAdmin() {
         if (!admins.isMember(msg.sender)) revert Unauthorized();
         _;
@@ -70,7 +79,10 @@ contract BZDAccessList {
     /// List Logic
     /// -----------------------------------------------------------------------
 
-    function listAccount(address[] calldata accounts) external onlyAdmin {
+    /// @notice Add accounts.
+    /// @param accounts An array of accounts to add to an access list.
+    function list(address[] calldata accounts) external onlyAdmin {
+        if (accounts.length == 0) revert NoAccountToAdd();
         unchecked {
             ++listCount;
         }
@@ -79,8 +91,8 @@ contract BZDAccessList {
 
         for (uint256 i = 0; i < length; ) {
 
-            lists[listCount].push(accounts[i]);
-            listed[listCount][accounts[i]] = true;
+            lists[listCount].list.push(accounts[i]);
+            lists[listCount].listed[accounts[i]] = true;
 
             unchecked {
                 ++i;
@@ -88,19 +100,46 @@ contract BZDAccessList {
         }
     }
 
-    function removeAccount(uint256 id, address account) external onlyAdmin {
-        if (lists[id].length < 1) revert NothingToRemove();
+    /// @notice Add accounts to an existing list.
+    /// @param id The identifier of an existing list.
+    /// @param accounts An array of accounts to add to an existing access list.
+    function updateList(uint256 id, address[] calldata accounts) external onlyAdmin {
+        if (accounts.length == 0) revert NoAccountToAdd();
 
-        uint256 length = lists[id].length;
+        (, , , uint256 _length) = this.getList(id);
+
+        if (_length > 0) {
+            uint256 length = accounts.length;
+
+            for (uint256 i = 0; i < length; ) {
+
+                lists[id].list.push(accounts[i]);
+                lists[id].listed[accounts[i]] = true;
+
+                unchecked {
+                    ++i;
+                }
+            }
+        } else {
+            revert ListNotFound();
+        }
+    }
+
+    /// @notice Remove single account from an existing list.
+    /// @param id The identifier of an existing list.
+    /// @param account An account to be removed from an existing access list.
+    function remove(uint256 id, address account) external onlyAdmin {
+        (, , address[] memory _lists, uint256 _length ) = this.getList(id);
+        if (_length < 1) revert NoAccountToRemove();
+
         address accountToRemove;
 
-        for (uint256 i = 0; i < length; ) {
-
-            accountToRemove = lists[id][i];
+        for (uint256 i = 0; i < _length; ) {
+            accountToRemove = _lists[i];
             
             if (accountToRemove == account) {
-                lists[id][i] = address(0);
-                listed[id][account] = false;
+                delete lists[id].list[i];
+                delete lists[listCount].listed[accountToRemove];
             }
 
             unchecked {
@@ -109,44 +148,67 @@ contract BZDAccessList {
         }
     }
 
-    function setMintPrice(uint256 id, uint256 fee) external onlyAdmin {
-        listMintFee[id] = fee;
+    /// @notice Update mint price per access list.
+    /// @param id The identifier of an access list.
+    /// @param _fee The fee required to mint a BZDMembershipNFT.
+    function setMintPrice(uint256 id, uint256 _fee) external onlyAdmin {
+        lists[id].fee = _fee;
+    }
+
+    /// @notice Allow an access list to start minting BZDMembershipNFTs.
+    /// @param id The identifier of a list.
+    /// @param _open Status to mint a BZDMembershipNFT.
+    function startMint(uint256 id, bool _open) external onlyAdmin {
+        lists[id].open = _open;
     }
 
     /// -----------------------------------------------------------------------
     /// Mint Logic
     /// -----------------------------------------------------------------------
 
-    function mint(uint256 id, uint256 _seasonId) external payable {
-        if (!listed[id][msg.sender]) revert NotListed();
-        if (listMintFee[id] != msg.value) revert IncorrectAmount();
+    /// @notice Allow an access list to start minting BZDMembershipNFTs.
+    /// @param id The identifier of an access list.
+    /// @param seasonId The season to mint a BZDMembershipNFT.
+    function mint(uint256 id, uint256 seasonId) external payable {
+        (bool _open, uint256 _fee, ,) = this.getList(id);
+        (bool isListed) = this.verifyAccount(id, msg.sender);
 
-        // Might look into creating a new function at BZDMembershipNFT to add single member
-        address[] memory _account = new address[](1);
-        _account[1] = msg.sender;
+        if (!isListed) revert NotListed();
+        if (!_open) revert MintHasNotStarted();
+        if (_fee != msg.value) revert IncorrectAmount();
 
-        nft.mintAndAddMembersToSeason(_account, _seasonId);
+        nft.mintAndAddMembersToSeason(msg.sender, seasonId);
     }
 
     /// -----------------------------------------------------------------------
     /// Getter Logic
     /// -----------------------------------------------------------------------
 
-    function verify(uint256 id, address account) external view returns (bool isListed) {
-        return listed[id][account];
+    function verifyAccount(uint256 id, address account) external view returns (bool isListed) {
+        return lists[id].listed[account];
     }
 
-    function getList(uint256 id) external view returns (address[] memory, uint256) {
-        return (lists[id], lists[id].length);
+    function getList(uint256 id) external view returns (bool, uint256, address[] memory, uint256) {
+        return (lists[id].open, lists[id].fee, lists[id].list, lists[id].list.length);
     }
 
     /// -----------------------------------------------------------------------
     /// Admin Logic
     /// -----------------------------------------------------------------------
 
+    /// @notice Update contract addresses.
+    /// @param _admins The address of a BZDMembershipDirectory contract.
+    /// @param _nft The address of a BZDMembershipNFT contract.
     function updateContract(IBZDMembershipDirectory _admins, IBZDMembershipNFT _nft) external onlyAdmin {
         admins = _admins;
         nft = _nft;
+    }
+    
+    /// @notice Withraw funds from this BZDAccessList contract.
+    /// @param to The recipient to send funds to.
+    function withdraw(address payable to) external onlyAdmin {
+        (bool success, ) = to.call{value: address(this).balance}("");
+        if (!success) revert WithdrawalFailed();
     }
 
     receive() external payable {}
